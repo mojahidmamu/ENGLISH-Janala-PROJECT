7a6c0afef7b9688cf440a79b27f568cb78739bce
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-27 16:01:08 +0600

    addded new

--- src/components/VocabularySection/VocabularySection.jsx
+++ src/components/VocabularySection/VocabularySection.jsx
@@ -138,7 +138,7 @@ const VocabularySection = () => {
               className="px-4 py-2 bg-blue-500 text-white rounded-lg"
               onClick={() => setSelectedWord(null)}
             >
-              Close
+              Completing Learning
             </button>
           </div>
         </div>
