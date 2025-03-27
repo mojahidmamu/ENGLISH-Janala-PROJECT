ef197105152ea21b5908df0814bb4910dcdbfefb
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-03-27 16:33:01 +0600

    addded new

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import OngoingProject from "../OngoingProject/OngoingProject";
 
 const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <OngoingProject></OngoingProject>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
